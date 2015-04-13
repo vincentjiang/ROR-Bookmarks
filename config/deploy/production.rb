62534ecccc721966cb0a580e167f532b27d73483
zwt <357557688@qq.com>
2015-04-13 22:28:26 +0800

    add secret_key_base

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,7 @@ set :enable_ssl, false
 # PUMA
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,   "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix:/opt/app/shared/tmp/sockets/puma.sock"
+set :puma_bind, "unix:/opt/project/bookmark/shared/tmp/sockets/puma.sock"
 set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log"
